a82ef7247633c5fbbe9b1e26a825558129713429
HC <hc@vst.io>
2019-01-05 17:29:25 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tradeioapi',
+    version='0.1',
+    packages=['tradeioapi', 'tradeioapi.api', 'tradeioapi.test'],
+    url='',
+    license='',
+    author='hcv57',
+    author_email='',
+    description=''
+)
